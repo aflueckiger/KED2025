fedefbd7c4f668606266e00a98f0e82ccdbc286e
aflueckiger <aflueckiger@users.noreply.github.com>
2025-01-12 16:00:32 +0100

    update code samples

--- ked/materials/code/text_annotator.py
+++ ked/materials/code/text_annotator.py
@@ -1,82 +1,4 @@
-# %%
-import spacy
-import pandas as pd
-from umap import UMAP
-from sentence_transformers import SentenceTransformer
-
-
-# load German language model
-nlp = spacy.load("de_core_news_sm")
-
-# Load dataframe with a parsed date column
-df = pd.read_csv("../data/extended_swiss_media_wokeness.tsv", parse_dates=["pubtime"])
-
-df.rename(columns={"content": "text"}, inplace=True)
-
-# process documents efficiently (batch-wise)
-df["doc"] = list(nlp.pipe(df["text"]))
-
-df.head()
-
-# %%
-
-def transform_into_sentlevel_dataframe(df):
-    data = []
-    print("Number of documents:", len(df))
-    for idx, row in df.iterrows():
-
-        meta = row.to_dict()
-        del meta["doc"]
-        del meta["text"]
-
-        for sent in row["doc"].sents:
-            data_sent = {**meta}
-            data_sent["text"] = sent.text
-            data.append(data_sent)
-
-    df_sent = pd.DataFrame(data)
-    print("Number of sentences:", len(df_sent))
-
-    return df_sent
-
-# create a sentence-level dataframe
-df_sent = transform_into_sentlevel_dataframe(df)
-
-# set meta information to display in the interface (date is formatted)
-df_sent["meta"] = df_sent["pubtime"].dt.strftime('%m/%d/%Y').astype(str) + ", " + df_sent["medium_code"]
-
-
-# select only sentence with a particular term
-# df_sent = df_sent[df_sent["text"].str.contains("woke", case=False)]
-
-# filter out sentences with less than 15 characters
-df_sent = df_sent[df_sent["text"].str.len() >= 15]
-df_sent = df_sent.sample(5000, random_state=42)
-
-
-
-# %%
-# Build a sentence encoder pipeline with UMAP at the end.
-
-# Calculate embeddings
-model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2", trust_remote_code=True)
-X_embeddings = model.encode(df_sent["text"].values)
-
-# model = SentenceTransformer("jinaai/jina-embeddings-v3", trust_remote_code=True)
-# task = "text-matching"
-# X_embeddings = model.encode(sentences, task=task, prompt_name=task, truncate_dim=256)
-
-# Reduce dimensions
-umap = UMAP(random_state=42)
-X_tfm = umap.fit_transform(X_embeddings)
-
-# Add the reduced dimensions to the dataframe
-df_sent["x"] = X_tfm[:, 0]
-df_sent["y"] = X_tfm[:, 1]
-
-# %%
 import jscatter
-import numpy as np
 import pandas as pd
 from IPython.display import display
 from ipywidgets import HTML, Button, HBox, Layout, Text, VBox, Select
@@ -94,6 +16,8 @@ class BaseTextAnnotator:
         self.X = X
         self.encoder = encoder
 
+        self.size = 5
+
         self.init_labeling()
         self.create_widget()
 
@@ -107,6 +31,8 @@ class BaseTextAnnotator:
             color_by="label",
             width=500,
             height=500,
+            axes=False,
+            size=self.size,
         )
 
         # query input
@@ -155,12 +81,12 @@ class BaseTextAnnotator:
 
     def color_by_label(self):
         self.scatter.color(by=self.dataf["label"])
-        self.scatter.size(1)
+        self.scatter.size(self.size)
 
     def labeling(self, change):
         # filter as event gets triggered multiple times
         if change["new"] in self.labels:
-            self.dataf.loc[widget.selected_idx, "label"] = change["new"]
+            self.dataf.loc[self.scatter.selection, "label"] = change["new"]
             self.color_by_label()
 
     def show(self):
@@ -179,7 +105,8 @@ class BaseTextAnnotator:
                 </p>"""
             for t, l, m in zip(rows["text"], rows["label"], rows["meta"])
         ]
-        self.html.value = "".join(sorted(texts))
+        n_selected_text = f"<p><b>Number of selected texts: {len(self.scatter.selection())}<b><p>"
+        self.html.value = n_selected_text + "".join(sorted(texts))
 
     def update_query(self, change):
         if self.text_query.value:
@@ -190,8 +117,16 @@ class BaseTextAnnotator:
             # norm_dists = 0.01 + (dists - dists.min()) / (
             #     0.1 + dists.max() - dists.min()
             # )
-            self.scatter.color(by=norm_dists[0])
-            self.scatter.size(by=norm_dists[0])
+            self.scatter.color(by=norm_dists[0], map='magma')
+            # self.scatter.size(by=norm_dists[0])
+
+            # self.scatter.opacity(by=norm_dists[0])
+
+            self.scatter.color(labeling={
+                "variable": "cosine similarity",
+                "minValue": "min",
+                "maxValue": "max", 
+            })
         else:
             # reset if query is empty
             self.color_by_label()
@@ -216,23 +151,3 @@ class BaseTextAnnotator:
 
     def save_data(self, path):
         self.dataf.to_csv(path, index=False)
-
-
-labels = ["label1", "label2", "label3"]
-widget = BaseTextAnnotator(df_sent, X=X_embeddings, encoder=model, labels=labels)
-widget.show()
-
-# %%
-widget.save_data("labeled_data.csv")
-
-# %%
-import datamapplot
-
-datamapplot.create_plot(
-    X_tfm,
-    widget.dataf["label"],
-    title="Map of Embeddings",
-    sub_title="Each sentence represents a point in the semantic space",
-)
-# %%
-
